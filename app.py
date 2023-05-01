95b3a0babfd55a2c52fbe6a5d7d6f5309a6a851a
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-05-01 12:32:45 -0700

    create Cupcake class and create routes to retrieve cupcake data

--- app.py
+++ app.py
@@ -1,1 +1,63 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, jsonify, request
+
+from models import connect_db, db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcakes")
+
+connect_db(app)
+
+@app.get("/api/cupcakes")
+def get_cupcakes():
+    """Retrieve data about all cupcakes and respond with JSON"""
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.get("/api/cupcakes/<int:id>")
+def get_cupcake(id):
+    """
+    Retrieve data about a single cupcake
+    Respond with JSON like: {cupcake: {id, flavor, size, rating, image_url}}
+    """
+
+    cupcake = Cupcake.query.get_or_404(id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post("/api/cupcakes")
+def create_cupcake():
+    """
+    Create a cupcake
+    Respond with JSON like: {cupcake: {id, flavor, size, rating, image_url}}
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image_url = request.json["image_url"] or None
+
+    cupcake = Cupcake(
+        flavor=flavor,
+        size=size,
+        rating=rating,
+        image_url = image_url
+    )
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    serialized = cupcake.serialize()
+
+    return (jsonify(cupcake=serialized), 201)
--- models.py
+++ models.py
@@ -1,1 +1,60 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+class Cupcake(db.Model):
+    """create a cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False,
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False,
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        nullable=False,
+        default=DEFAULT_IMG_URL,
+    )
+
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image_url": self.image_url,
+        }
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
