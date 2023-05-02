c19b05860530f71240a8d7d62237a4f6df5b3ad2
Steven <stzheng716@gmail.com>
2023-05-01 17:07:38 -0700

    handle new cupcake form

--- static/ui.js
+++ static/ui.js
@@ -3,6 +3,7 @@
 const BASE_API_URL = "http://localhost:5001/api/"
 
 const $cupcake = $("#cupcakeList");
+const $newCupcakeForm = $("#addCupcake");
 
 /** Get the cupcakes from the API */
 
@@ -16,6 +17,8 @@ async function getCupcakes() {
 /** Populate the list of cupcakes */
 
 async function cupcakesList() {
+  $cupcake.empty()
+
   const cupcakes = await getCupcakes();
 
   for (let cupcake of cupcakes) {
@@ -24,5 +27,26 @@ async function cupcakesList() {
     $cupcake.append($li);
   }
 }
+
+/** Add cupcake to API and update cupcake list */
+
+async function addNewCupcake(evt) {
+  evt.preventDefault()
+
+  const flavor = $("#flavor").val();
+  const size = $("#size").val();
+  const rating = $("#rating").val();
+  const image_url = $("#image_url").val();
+
+  const newCupcake = { flavor, size, rating, image_url }
+
+  await axios.post(
+    `${BASE_API_URL}cupcakes`, newCupcake);
+
+  cupcakesList()
+
+}
+
+$newCupcakeForm.on("submit", addNewCupcake);
 
 cupcakesList();
